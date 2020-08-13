0bd52c667271ba27cfb6c855d0d68ba0eb9b48fc
pvpatil5 <pvpatil123@yahoo.com>
2020-08-13 17:33:34 +0530

    Third commit making changes to existing file

--- src/test/java/test/Sample1.java
+++ src/test/java/test/Sample1.java
@@ -7,11 +7,7 @@ public class Sample1 {
 
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.chrome.driver" ,"../SeleniumAuto/src/main/resources/DriverExe/chromedriver.exe" );
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
-		System.out.println(driver.getTitle());
-
+		System.out.println(" Thrid commit ");
 
 	}
 
